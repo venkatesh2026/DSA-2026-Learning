c9a7ce5a9e54133770e83baf6ee74185b757261d
venkatesh-dabbara <157109408+venkatesh-dabbara@users.noreply.github.com>
2026-05-17 16:21:50 +0530

    remove duplicates from sorted array

---
+++ src/main/java/RemoveDuplicateElementsFromSortedArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class RemoveDuplicateElementsFromSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,1,2,2,3,4,4,5,6,6,7};
+        System.out.println(removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int slow = 0, fast = 1;
+        while (fast < nums.length) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow + 1;
+    }
+}
