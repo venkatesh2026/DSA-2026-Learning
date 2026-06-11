d46d1e291ac15112a9c97fb37541176e7dc8cfe7
venkatesh-dabbara <157109408+venkatesh-dabbara@users.noreply.github.com>
2026-06-11 13:22:38 +0530

    two pointer approach

--- src/main/java/TrappingRainWater.java
+++ src/main/java/TrappingRainWater.java
@@ -58,4 +58,30 @@ public class TrappingRainWater {
         }
         return totalWater;
     }
+
+    public static int trapTwoPointerApproach(int[] height) {
+
+        int left = 0, right = height.length - 1;
+        int maxLeft = 0;
+        int maxRight = 0;
+        int totalWater = 0;
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    totalWater = totalWater + (maxLeft - height[left]);
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    totalWater = totalWater + (maxRight - height[right]);
+                }
+                right--;
+            }
+        }
+        return totalWater;
+    }
 }
