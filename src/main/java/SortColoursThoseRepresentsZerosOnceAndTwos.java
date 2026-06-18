273581ec04dbafdf6e3ab414ecd105251c670fdc
venkatesh-dabbara <157109408+venkatesh-dabbara@users.noreply.github.com>
2026-06-18 13:17:26 +0530

    sort array contains 0,1 and 2 with O(n) Tc

--- src/main/java/SortColoursThoseRepresentsZerosOnceAndTwos.java
+++ src/main/java/SortColoursThoseRepresentsZerosOnceAndTwos.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 public class SortColoursThoseRepresentsZerosOnceAndTwos {
 
     public static void main(String[] args) {
-        int[] nums = {2, 0, 2, 1, 1, 0};
-        sortColors(nums);
+        int[] nums = {2, 2};
+        sortColoursSinglePass(nums);
         System.out.println(Arrays.toString(nums));
 
     }
 
-    public static void sortColors(int[] nums) {
+    public static void sortColorsCountApproach(int[] nums) {
 
         int zerosCount = 0, onceCount = 0, twosCount = 0;
 
@@ -34,6 +34,86 @@ public class SortColoursThoseRepresentsZerosOnceAndTwos {
                 twosCount--;
             }
         }
+    }
 
+    // in first pass move zeros to starting
+    //in second pass move 1's after zeros
+    public static void sortColoursWithTwoPointerAndTwoPass(int[] nums) {
+        int nextZeroIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                if (i != nextZeroIndex) {
+                    int temp = nums[nextZeroIndex];
+                    nums[nextZeroIndex] = nums[i];
+                    nums[i] = temp;
+                }
+                nextZeroIndex++;
+            }
+        }
+
+        int nextOneIndex = nextZeroIndex;
+        for (int i = nextZeroIndex; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (i != nextOneIndex) {
+                    int temp = nums[nextOneIndex];
+                    nums[nextOneIndex] = nums[i];
+                    nums[i] = temp;
+                }
+                nextOneIndex++;
+            }
+        }
+    }
+
+    public static void sortColoursSinglePass(int[] nums) {
+        int zerosIndex = 0, twosIndex = nums.length - 1;
+
+        int i = 0;
+
+        while (i <= twosIndex) {
+
+            if (nums[i] == 0) {
+                if (i != zerosIndex) {
+                    int temp = nums[i];
+                    nums[i] = nums[zerosIndex];
+                    nums[zerosIndex] = temp;
+                }
+                zerosIndex++;
+                i++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                while (i <= twosIndex && nums[twosIndex] == 2) {
+                    twosIndex--;
+                }
+                if (i <= twosIndex) {
+                    int temp = nums[i];
+                    nums[i] = nums[twosIndex];
+                    nums[twosIndex] = temp;
+                    twosIndex--;
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
